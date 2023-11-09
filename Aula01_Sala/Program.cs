0faed90bc883962e8392c4bf45754bf577c71195
GiuseppeMota <giuseppemota@gmail.com>
2023-11-09 16:52:11 -0300

    estudando tipos

--- Aula01_Sala/Program.cs
+++ Aula01_Sala/Program.cs
@@ -1,1 +1,18 @@
 ﻿Console.WriteLine("Hello, World .NET");
+
+#region Teste de Tipos deDados
+
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+    tipoDouble = double.MaxValue;
+
+    Console.WriteLine("O máximo inteiro é: " + tipoInteiro);
+    Console.WriteLine("O máximo long é: " + tipoLong);
+    Console.WriteLine("O máximo double é: " + tipoDouble);
+
+#endregion

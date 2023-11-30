0a07df4186020151b19b09f345e1fe887d104f0c
GiuseppeMota <giuseppemota@gmail.com>
2023-11-30 17:25:06 -0300

    Ajuste v1.3

--- ProvaIndividual/Program.cs
+++ ProvaIndividual/Program.cs
@@ -32,11 +32,18 @@ escritorio.AdicionarCliente(pedro);
 //escritorio.RemoverAdvogado(giuseppe);
 //escritorio.RemoverCliente(henrique);
 
+System.Console.WriteLine("Relatorio: ");
+System.Console.WriteLine("Advogados com idade entre 18 e 30 anos: ");
 Relatorio.AdvogadoIdadeEntre(escritorio.Advogados, 18, 30);
+System.Console.WriteLine("Clientes com idade entre 18 e 30 anos: ");
 Relatorio.ClienteIdadeEntre(escritorio.Clientes, 18, 30);
+System.Console.WriteLine("Clientes com profissão: ");
 Relatorio.ClienteComProfissao(escritorio.Clientes, "Developer");
+System.Console.WriteLine("Clientes com estado civil: ");
 Relatorio.ClienteEstadoCivil(escritorio.Clientes);
+System.Console.WriteLine("Clientes em ordem alfabetica: ");
 Relatorio.ClienteOrdemAlfabetica(escritorio.Clientes);
+System.Console.WriteLine("Pessoas aniversariantes do mes: ");
 Relatorio.PessoasAniversariantes(escritorio.Advogados, escritorio.Clientes, 10);
 
 

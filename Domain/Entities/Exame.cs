2c1e21de11b68303e27cbf50afb0ed85a39872cc
GiuseppeMota <giuseppemota@gmail.com>
2024-02-22 14:04:48 -0300

    initial commit

--- Domain/Entities/Exame.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Entities;
-
-public class Exame
-{
-    public int ExameId { get; set; }
-    public string? Local { get; set; }
-    public DateTime DataHora { get; set; }
-    public required Atendimento Atendimento { get; set; }
-}

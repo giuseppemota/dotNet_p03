2fc41cc085d82eb006f23adfe5b7cd881b2094b7
Giuseppe Mota <giuseppemota@gmail.com>
2024-03-22 23:41:44 -0300

    Update project file paths and cache files

---
+++ Atividade_CRUD_complete/src/Cepedi.Data/Repositories/CursoRepository.cs
@@ -0,0 +1,40 @@
+﻿using Cepedi.Domain;
+using Cepedi.Domain.Entities;
+using Cepedi.Shareable;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cepedi.Data;
+
+public class CursoRepository : ICursoRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CursoRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<CursoEntity> ObtemCursoPorIdAsync(int idCurso)
+    {
+        return await _context.Curso.Where(curso => curso.Id == idCurso).FirstOrDefaultAsync();
+    }
+    public Task<int> AddCursoDbAsync(CursoEntity entity)
+    {
+        _context.Curso.Add(entity);
+        return _context.SaveChangesAsync();
+    }
+
+    public async Task<CursoEntity> AlterarCursoDbAsync(CursoEntity entity)
+    {
+        _context.Curso.Update(entity);
+        await _context.SaveChangesAsync();
+        return ObtemCursoPorIdAsync(entity.Id).Result;
+    }
+
+    public async Task<CursoEntity> DeletarCursoDbAsync(CursoEntity entity){
+        _context.Curso.Remove(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+}
---
+++ Atividade_CRUD_complete/src/Cepedi.Domain/Entities/ProfessorEntity.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Cepedi.Domain.Entities;
+public class ProfessorEntity
+{
+    public ProfessorEntity(int id, string nome, string especialidade)
+    {
+        Id = id;
+        Nome = nome;
+        Especialidade = especialidade;
+    }
+
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = default!;
+    
+    public string Especialidade { get; set; } = default!;
+
+    public List<CursoEntity> Cursos { get; set; } = new();
+
+    public static implicit operator ProfessorEntity(Task<ProfessorEntity> v)
+    {
+        throw new NotImplementedException();
+    }
+}
